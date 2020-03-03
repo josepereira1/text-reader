a3a996ef84d763ae50411091caab6777350cd967
josepereira1 <joseandremp1@gmail.com>
2020-03-03 22:22:47 +0000

    documentation

--- text-reader.c
+++ text-reader.c
@@ -6,7 +6,7 @@
 #include "text-reader.h"
 
 /**
-\brief This funciton use system call <say> and read file with name given as argument.
+\brief This function use system call <say> and read file with name given as argument.
 @param filename - name of file
 @param size - maximum size of file
 */
